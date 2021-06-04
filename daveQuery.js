8f6aa07b90469dc1e971e8a6446dc5eed2de4894
David Domminney Fowler <mail@daviddomminney.com>
2021-06-04 05:59:41 +0100

    added UI and some extra insert stuff

--- daveQuery.js
+++ daveQuery.js
@@ -6,6 +6,7 @@ class ElementCollection extends Array {
     // events
 
     on(evt, fn) {
+
         this.forEach((elem) => {
             // check if function is really string and turn to function
             if (typeof fn === 'string' || fn instanceof String) {
@@ -17,6 +18,13 @@ class ElementCollection extends Array {
     }
 
     click(fn) {
+        if (fn === undefined) {
+            this.forEach((elem) => {
+                var evt = new MouseEvent("click")
+                elem.dispatchEvent(evt)
+            })
+            return this
+        }
         this.on("click", fn)
         return this
     }
@@ -31,17 +39,36 @@ class ElementCollection extends Array {
     }
 
     removeClass(cls) {
+        if (!Array.isArray(cls)) cls = [cls]
         this.forEach((elem) => {
-            elem.classList.remove(cls)
+            cls.forEach((clsElem) => {
+                elem.classList.remove(clsElem)
+            })
         })
         return this
     }
 
     addClass(cls) {
+        if (!Array.isArray(cls)) cls = [cls]
         this.forEach((elem) => {
-            elem.classList.add(cls)
+            cls.forEach((clsElem) => {
+                elem.classList.add(clsElem)
+            })
+
         })
         return this
+    }
+
+    hasClass(cls) {
+        var className = " " + cls + " "
+        var has = false;
+        this.forEach((elem) => {
+            if ((" " + elem.className + " ").replace(/[\n\t\r]/g, " ").indexOf(className) > -1) {
+                has = true
+                return true
+            }
+        })
+        return has
     }
 
     // css
@@ -66,7 +93,6 @@ class ElementCollection extends Array {
     append(appendObj) {
         this.forEach((elem) => {
             var aoa = $(appendObj)
-            console.log(aoa)
             aoa.forEach((ao) => {
                 elem.append(ao)
 
@@ -92,6 +118,15 @@ class ElementCollection extends Array {
 
     prependTo(prependToObj) {
         $(prependToObj).prepend(this)
+        return this
+    }
+
+    insertAfter(iaObj) {
+        iaObj = $(iaObj)
+        this.forEach((elem) => {
+            iaObj[0].parentNode.insertBefore(elem, iaObj[0].nextSibling)
+            iaObj[0] = elem
+        })
         return this
     }
 
@@ -136,12 +171,16 @@ class ElementCollection extends Array {
         return $(this[0].lastElementChild)
     }
 
-    find(selector){
-        var selected=new ElementCollection([])
-        this.forEach((elem)=>{            
-            selected=selected.concat($(selector,elem))
+    find(selector) {
+        var selected = new ElementCollection()
+        this.forEach((elem) => {
+            selected = selected.concat($(selector, elem))
         })
         return selected
+    }
+
+    parent() {
+        return $(this[0].parentNode)
     }
 
     // text & html content
@@ -177,24 +216,144 @@ class ElementCollection extends Array {
 
     // form
 
-    val(val){
-        if (typeof val === 'undefined'){
-            if (this.length){
+    val(val) {
+
+        if (typeof val === 'undefined') {
+            if (this[0].type === 'checkbox') {
+                if (this[0].checked) return 1
+                return 0
+            }
+            if (this.length) {
                 return this[0].value.toString()
             }
             return ''
         }
-        if (this.length){
-            this[0].value=val
-        }
+
+
+        this.forEach((elem) => {
+            var ovu = $(elem).data("dq-onvalupdate")
+            if (elem.type === 'checkbox') {
+                elem.checked = val
+                if (ovu !== '') {
+                    var fn = new Function('elem', ovu)
+                    setTimeout(fn, 0, elem)
+                }
+                return false
+            }
+            elem.value = val
+        })
+
         return this
+    }
+
+    // display & animations
+
+    show() {
+
+        this.forEach((elem) => {
+            elem = $(elem)
+            var disp = elem.data("dq-style-display")
+            if (disp === '') disp = 'block'
+            elem[0].style.display = disp
+        })
+
+        return this
+    }
+
+    hide() {
+
+        this.forEach((elem) => {
+            elem = $(elem)
+            var disp = elem[0].style.display
+            if (disp === '') disp = 'block'
+            elem.data("dq-style-display", disp)
+            elem[0].style.display = 'none'
+        })
+
+        return this
+    }
+
+    // UI
+
+    toToggle() {
+
+        this.forEach((input) => {
+
+            input = $(input)
+
+            if (input[0]?.type !== 'checkbox') return false
+            if (input.parent().hasClass("dq--formitem")) return false
+
+            var container = $("<div />").addClass(["dq--toggle--container", "dq--formitem"]).insertAfter(input)
+            container.append(input).append($("<div class='dq--toggle--switch' />"))
+            if (input.val()) container.addClass("dq--checked")
+
+            input.hide()
+
+            container.click(function () {
+                $(this).find("input").click()
+            })
+
+            var label=$("<label />").text(input.data("label")).prependTo(container)
+            
+            input.data("dq-onvalupdate", "elem.dispatchEvent(new Event('change'))")
+            
+            input.on("change", function () {
+                if (this.checked) {
+                    $(this).parent().addClass("dq--checked")
+                    return false
+                }
+                $(this).parent().removeClass("dq--checked")
+            })
+
+        })
+
+        return this
+
+    }
+
+    toTick() {
+
+        this.forEach((input) => {
+
+            input = $(input)
+
+            if (input[0]?.type !== 'checkbox') return false
+            if (input.parent().hasClass("dq--formitem")) return false
+
+            var container = $("<div />").addClass(["dq--tick--container", "dq--formitem"]).insertAfter(input)
+            container.append(input).append($("<div class='dq--tick--switch' />"))
+            if (input.val()) container.addClass("dq--checked")
+
+            input.hide()
+
+            container.click(function () {
+                $(this).find("input").click()
+            })
+
+            var label=$("<label />").text(input.data("label")).prependTo(container)
+            
+            input.data("dq-onvalupdate", "elem.dispatchEvent(new Event('change'))")
+            
+            input.on("change", function () {
+                if (this.checked) {
+                    $(this).parent().addClass("dq--checked")
+                    return false
+                }
+                $(this).parent().removeClass("dq--checked")
+            })
+            
+        })
+
+        return this
+
     }
 
 }
 
-var $ = function (selector,from) {
+var $ = function (selector, from) {
 
-    from=from ?? document
+    from = from ?? document
 
     if (typeof selector === 'string' || selector instanceof String) {
         if (selector.startsWith("<")) {
@@ -205,7 +364,7 @@ var $ = function (selector,from) {
         // is selector string
         return new ElementCollection(...from.querySelectorAll(selector))
     }
-    if (!Array.isArray(selector)) selector=new ElementCollection(selector)
+    if (!Array.isArray(selector)) selector = new ElementCollection(selector)
     // is already an array
     return new ElementCollection(...selector)
 }
